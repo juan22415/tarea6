e2c995532dfeef0c137ecefa8574846d397c3c14
juan camilo rios <juan22415@hotmail.com>
2017-03-06 23:40:46 -0500

    new

---
+++ Assets/Mind.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mind : MonoBehaviour
+{
+    private static Mind instance;
+
+    public static Mind Instance
+    {
+        get
+        {
+            return Instance;
+        }
+    }
+
+    private void Awake()
+    {
+        if (instance != null)
+            Destroy(gameObject);
+        else
+
+            instance = this;
+    }
+	
+	void Update () {
+		
+	}
+
+    public void updatesoldier (soldier activesoldier)
+    {
+        if (activesoldier.iscoliding == true)
+        {
+            activesoldier.atack();
+
+        }
+
+        else
+
+            activesoldier.move();
+
+    }
+}
---
+++ Assets/soldier.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class soldier : MonoBehaviour {
+
+    public float Health=100;
+    public float Attack;
+    public float Defence;
+    public bool iscoliding=false;
+    
+
+    private SpriteRenderer rederer;
+
+    public Mind themind;
+     
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            OnTurn();
+        }
+    }
+
+    public void OnTurn()
+    {
+        themind.updatesoldier(this);
+    }
+
+    public void atack()
+    {
+        Debug.Log("attack");
+        iscoliding = false;
+    }
+
+    public void move ()
+    {
+
+        Debug.Log("move");
+    }
+
+    public void stay()
+    {
+        Debug.Log("stay");
+    }
+
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        iscoliding = true;
+        
+    }
+}
